f8a05a117483edae0e5965be0c4b71968e2f84eb
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-10-26 19:39:27 +0300

    add async def blink

--- game.py
+++ game.py
@@ -1,24 +1,32 @@
-import time
 import curses
+import asyncio
+import time
+
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
 
 def draw(canvas):
     row, column = (10, 20)
     canvas.border()
     curses.curs_set(False)
+    coroutine = blink(canvas, row, column)
     while True:
-        canvas.addstr(row, column, '*', curses.A_DIM)
+        coroutine.send(None)
         canvas.refresh()
-        time.sleep(2)
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        canvas.refresh()
-        time.sleep(0.5)
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
+        time.sleep(1)
 
 
 def main():
