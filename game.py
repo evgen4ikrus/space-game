5b04768d4ebad99bcce52a5c69a5179ad87fd8f3
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-12-02 05:12:07 +0300

    fix smooth rocket movement

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ import random
 import time
 from itertools import cycle
 
-from curses_tools import draw_frame, get_frame_size, read_controls
+from curses_tools import draw_frame, get_frame_size, read_controls, update_speed
 
 
 async def sleep(tics=1):
@@ -63,30 +63,37 @@ def get_animations_frames(path):
     return animations_frames
 
 
-def get_new_rocket_coordinates(canvas, rocket_frame, rocket_row, rocket_column, rows_direction,
-                               columns_direction, canvas_border_indent):
+def get_new_rocket_coordinates(canvas, rocket_frame, rocket_row, rocket_column,
+                               rows_direction, columns_direction,
+                               canvas_border_indent, row_speed, column_speed):
     frame_rows, frame_columns = curses.window.getmaxyx(canvas)
-    rocket_row += rows_direction
-    rocket_column += columns_direction
+    row_speed, column_speed = update_speed(row_speed, column_speed, rows_direction, columns_direction)
+    rocket_row += row_speed
+    rocket_column += column_speed
     rocket_height, rocket_width = get_frame_size(rocket_frame)
-    if rocket_row <= 0:
+    if rocket_row <= 1:
         rocket_row = canvas_border_indent
-    if rocket_row >= frame_rows - rocket_height:
+    if rocket_row >= frame_rows - rocket_height - canvas_border_indent:
         rocket_row = frame_rows - rocket_height - canvas_border_indent
-    if rocket_column <= 0:
+    if rocket_column <= 1:
         rocket_column = canvas_border_indent
     if rocket_column >= frame_columns - rocket_width - canvas_border_indent:
         rocket_column = frame_columns - rocket_width - canvas_border_indent
-    return rocket_row, rocket_column
+    return rocket_row, rocket_column, row_speed, column_speed
 
 
 async def animate_spaceship(canvas, rocket_frames, rocket_row, rocket_column,
                             canvas_border_indent):
+    row_speed = column_speed = 0
     for rocket_frame in cycle(rocket_frames):
         rows_direction, columns_direction, _ = read_controls(canvas)
-        rocket_row, rocket_column = get_new_rocket_coordinates(
-            canvas, rocket_frame, rocket_row, rocket_column, rows_direction,
-            columns_direction, canvas_border_indent)
+        rocket_row, rocket_column, row_speed, column_speed = get_new_rocket_coordinates(
+            canvas, rocket_frame,
+            rocket_row, rocket_column,
+            rows_direction, columns_direction,
+            canvas_border_indent,
+            row_speed, column_speed
+        )
         draw_frame(canvas, rocket_row, rocket_column, rocket_frame)
         await asyncio.sleep(0)
         draw_frame(canvas, rocket_row, rocket_column, rocket_frame, negative=True)
